5daa0af56e44295c0580191d0ca372a44ef182ee
Ricardo <ricardotrivino@capitole-consulting.com>
2019-03-20 10:42:48 +0100

    Little refactoring in AndroidSearchPageObject. Problem with WebApp is not reproducible in other computers. Must test in the computer where the bug was found to solve it.

--- src/test/com/trivinosanchez/test/features/pages/tabs/search/AndroidSearchPageObject.java
+++ src/test/com/trivinosanchez/test/features/pages/tabs/search/AndroidSearchPageObject.java
@@ -17,26 +17,34 @@ public class AndroidSearchPageObject extends PageObject implements SearchPageObj
     @FindBy(how = How.ID, using = "org.wikipedia:id/search_src_text")
     private WebElement searchBox;
 
+    @FindBy(how = How.XPATH, using = "//*[@resource-id='org.wikipedia:id/search_toolbar']/android.widget.ImageButton")
+    private WebElement backArrow;
+
     public AndroidSearchPageObject(WebDriver driver) {
         super(driver);
     }
 
     public void enterText(String text) {
-        waitUntilVisibilityOf(searchContainer);
-        searchContainer.click();
         waitUntilVisibilityOf(searchBox);
         searchBox.sendKeys(text);
     }
 
-
     public void submitSearch() {
-//        searchBox.sendKeys(Keys.ENTER);
         ((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.ENTER));
     }
 
-
     public void searchFor(String textToSearch) {
+        openSearchBox();
         enterText(textToSearch);
         submitSearch();
+    }
+
+    public void openSearchBox() {
+        waitUntilVisibilityOf(searchContainer);
+        searchContainer.click();
+    }
+
+    public void closeSearchBox() {
+        backArrow.click();
     }
 }
